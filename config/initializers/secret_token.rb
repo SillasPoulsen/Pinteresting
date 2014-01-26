c3133403f297b485b987f727297766a5a798bbcb
Silas Poulsen <sillas_2@hotmail.com>
2014-01-26 15:27:32 -0800

    devise

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,3 +10,4 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 Pinteresting::Application.config.secret_key_base = '947a5e267d86bf6d3e9308525894a5a29d0ef547582bea8a2310c84f6a5b204f7d2b823de5b7da57aef631cf6a3cfe20578c82e386002e4cde87f5a3b876bac8'
+
