d85c86db2161c2b53abb00635b5f37900d244850
Silas Poulsen <sillas_2@hotmail.com>
2014-09-12 00:09:06 +0200

    added jQuery Msonry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,14 @@ Pinteresting::Application.configure do
 #note to set this to acutal hostname
 config.action_mailer.default_url_options = { :host => 'http://sillas-pinteresting.herokuapp.com' }
 
+# This sets paperclip to upload pictures to amazon
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
